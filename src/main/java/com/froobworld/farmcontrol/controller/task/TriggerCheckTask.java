23a87dbe195d8a410d8e9227febaf6f05a0f5ace
Nahuel Dolores <contact@nahu.me>
2024-02-13 13:01:27 -0300

    Revert "Use non-validating entity looping"
    This reverts commit 18f17b97f599bad5cd108a339aafc3a7c8a262af.

--- src/main/java/com/froobworld/farmcontrol/controller/task/TriggerCheckTask.java
+++ src/main/java/com/froobworld/farmcontrol/controller/task/TriggerCheckTask.java
@@ -10,7 +10,6 @@ import com.froobworld.farmcontrol.controller.trigger.UntriggerStrategy;
 import com.froobworld.farmcontrol.hook.scheduler.ScheduledTask;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 
 import java.util.*;
@@ -63,11 +62,7 @@ public class TriggerCheckTask implements Runnable {
             List<SnapshotEntity> snapshotEntities = new ArrayList<>();
             CompletableFuture<Void> completableFuture = CompletableFuture.completedFuture(null);
             if (!profilesToRun.isEmpty() || !untriggerStrategyMap.isEmpty()) {
-                for (Entity rawEntity : world.getEntities()) {
-                    if (!(rawEntity instanceof Mob entity)) {
-                        continue;
-                    }
-
+                for (Mob entity : world.getEntitiesByClass(Mob.class)) {
                     CompletableFuture<Void> entityFuture = new CompletableFuture<>();
                     ScheduledTask scheduledTask = farmControl.getHookManager().getSchedulerHook().runEntityTaskAsap(() -> {
                         try {
